c35759215cdb7fe9d9c3e3ff05c9e7e27bdef81c
UyenPham <uyenpt@steadfast-inc.com>
2018-02-06 08:39:59 +0700

    compass

--- app/src/main/java/com/uyenpham/diploma/flashlight/view/fragment/SwitchFlashFragment.java
+++ app/src/main/java/com/uyenpham/diploma/flashlight/view/fragment/SwitchFlashFragment.java
@@ -1,5 +1,11 @@
 package com.uyenpham.diploma.flashlight.view.fragment;
 
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
@@ -7,8 +13,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.uyenpham.diploma.flashlight.R;
@@ -17,6 +26,7 @@ import com.uyenpham.diploma.flashlight.utils.PreferenceUtils;
 import com.uyenpham.diploma.flashlight.view.customview.CustomTimeDialog;
 import com.uyenpham.diploma.flashlight.view.customview.TextSeekbarView;
 
+import static android.content.Context.SENSOR_SERVICE;
 import static com.uyenpham.diploma.flashlight.utils.Const.KEY_TIME_DELAY;
 
 /**
@@ -24,13 +34,21 @@ import static com.uyenpham.diploma.flashlight.utils.Const.KEY_TIME_DELAY;
  */
 
 public class SwitchFlashFragment extends Fragment implements SeekBar.OnSeekBarChangeListener, View
-        .OnClickListener {
+        .OnClickListener, SensorEventListener {
     private SeekBar seekBar;
     private TextSeekbarView textSeekbarView;
+    private TextView tvCoordinates;
+    private TextView tvNotiNoCompass;
+    private ImageView imvCompass;
     private int progress;
-    private int blinkDelay =0;
+    private int blinkDelay = 0;
     private ImageView btnSwitch;
-    public static boolean isFlash =false;
+    public static boolean isFlash = false;
+    // record the compass picture angle turned
+    private float currentDegree = 0f;
+    // device sensor manager
+    private SensorManager mSensorManager;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable
@@ -38,14 +56,42 @@ public class SwitchFlashFragment extends Fragment implements SeekBar.OnSeekBarCh
         View view = LayoutInflater.from(getActivity()).inflate(
                 R.layout.fragment_main, null);
 
+        initView(view);
+        initData();
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_GAME);
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+
+    }
+
+    private void initView(View view) {
         seekBar = view.findViewById(R.id.seekbar);
         textSeekbarView = view.findViewById(R.id.seekbarText);
         btnSwitch = view.findViewById(R.id.button_switch);
+        tvCoordinates = view.findViewById(R.id.tvCoordinates);
+        tvNotiNoCompass = view.findViewById(R.id.tvNotiNoCompass);
+        imvCompass = view.findViewById(R.id.imvCompass);
 
         seekBar.setOnSeekBarChangeListener(this);
         btnSwitch.setOnClickListener(this);
         view.findViewById(R.id.btnSetting).setOnClickListener(this);
-        return view;
+    }
+
+    private void initData() {
+        checkCompass();
+        mSensorManager = (SensorManager) getActivity().getSystemService(SENSOR_SERVICE);
     }
 
     @Override
@@ -62,7 +108,7 @@ public class SwitchFlashFragment extends Fragment implements SeekBar.OnSeekBarCh
     public void onStopTrackingTouch(SeekBar seekBar) {
         seekBar.setProgress(convertProgress(progress) * 10);
         textSeekbarView.setEnableText(convertProgress(progress));
-        blinkDelay =1000 / convertProgress(progress);
+        blinkDelay = 1000 / convertProgress(progress);
         turnOnFlash(blinkDelay);
     }
 
@@ -94,10 +140,10 @@ public class SwitchFlashFragment extends Fragment implements SeekBar.OnSeekBarCh
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button_switch:
-                if(isFlash){
+                if (isFlash) {
                     FlashUtil.stopFlickerFlash();
-                    btnSwitch.setImageResource(R.mipmap.switch_off);
-                }else {
+                    btnSwitch.setImageResource(R.mipmap.btn_off);
+                } else {
                     turnOnFlash(blinkDelay);
                 }
                 break;
@@ -107,7 +153,7 @@ public class SwitchFlashFragment extends Fragment implements SeekBar.OnSeekBarCh
                     @Override
                     public void onClick(String tag) {
                         Toast.makeText(getActivity(), tag, Toast.LENGTH_SHORT).show();
-                        PreferenceUtils.saveInt(getActivity(),KEY_TIME_DELAY,convertTimeDelay(tag));
+                        PreferenceUtils.saveInt(getActivity(), KEY_TIME_DELAY, convertTimeDelay(tag));
                         customDialog.dismiss();
                     }
                 });
@@ -118,51 +164,108 @@ public class SwitchFlashFragment extends Fragment implements SeekBar.OnSeekBarCh
         }
     }
 
-    private void turnOnFlash(int delay){
-        btnSwitch.setImageResource(R.mipmap.switch_on);
-        if(blinkDelay == 0){
+    private void turnOnFlash(int delay) {
+        btnSwitch.setImageResource(R.mipmap.btn_on);
+        if (blinkDelay == 0) {
             FlashUtil.flashOn();
-        }else {
+        } else {
             FlashUtil.setBlinkDelay(delay);
             FlashUtil.flickerFlash(getActivity());
         }
         setTimeOff();
     }
-    private int convertTimeDelay(String timeStr){
+
+    private int convertTimeDelay(String timeStr) {
         int time;
-        if(compareString(timeStr,"10s")){
-            time = 10*1000;
-        }else if(compareString(timeStr, "20s")){
-            time = 20*1000;
-        }else if(compareString(timeStr, "1m")){
-            time = 60*1000;
-        }else if(compareString(timeStr, "3m")){
-            time =3*60*1000;
-        }else if(compareString(timeStr, "5m")){
-            time = 5*60*1000;
-        }else if(compareString(timeStr, "10m")){
-            time = 10*60*1000;
-        }else if(compareString(timeStr, "30m")){
-            time =30*60*1000;
-        }else {
-            time =0;
+        if (compareString(timeStr, "10s")) {
+            time = 10 * 1000;
+        } else if (compareString(timeStr, "20s")) {
+            time = 20 * 1000;
+        } else if (compareString(timeStr, "1m")) {
+            time = 60 * 1000;
+        } else if (compareString(timeStr, "3m")) {
+            time = 3 * 60 * 1000;
+        } else if (compareString(timeStr, "5m")) {
+            time = 5 * 60 * 1000;
+        } else if (compareString(timeStr, "10m")) {
+            time = 10 * 60 * 1000;
+        } else if (compareString(timeStr, "30m")) {
+            time = 30 * 60 * 1000;
+        } else {
+            time = 0;
         }
         return time;
     }
-    private   boolean compareString(String str1, String str2){
+
+    private boolean compareString(String str1, String str2) {
         return str1.equalsIgnoreCase(str2);
     }
 
-    private  void setTimeOff() {
+    private void setTimeOff() {
         int delay = PreferenceUtils.getInt(getActivity(), KEY_TIME_DELAY, 0);
         if (delay != 0) {
             new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
                     FlashUtil.stopFlickerFlash();
-                    btnSwitch.setImageResource(R.mipmap.switch_off);
+                    btnSwitch.setImageResource(R.mipmap.btn_off);
                 }
             }, delay);
         }
+    }
+
+    private void checkCompass() {
+        PackageManager pm = getActivity().getPackageManager();
+        if (!pm.hasSystemFeature(PackageManager.FEATURE_SENSOR_COMPASS)) {
+            // This device does not have a compass, turn off the compass feature
+            tvNotiNoCompass.setVisibility(View.VISIBLE);
+        } else {
+            tvNotiNoCompass.setVisibility(View.GONE);
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float degree = Math.round(sensorEvent.values[0]);
+        tvCoordinates.setText(getDerection(degree)+ " "+(int)(180-degree) +(char) 0x00B0);
+        // create a rotation animation (reverse turn degree degrees)
+        RotateAnimation ra = new RotateAnimation(
+                currentDegree, -degree,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        // how long the animation will take place
+        ra.setDuration(210);
+        // set the animation after the end of the reservation status
+        ra.setFillAfter(true);
+        imvCompass.startAnimation(ra);
+        currentDegree = -degree;
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+    private String getDerection(float de){
+        String derection ="";
+        if(de ==0){
+            derection = "N";
+        }else if(0<de && de <90){
+            derection = "NE";
+        }else if(de ==90){
+            derection ="E";
+        }else if(90<de && de <180){
+            derection = "ES";
+        }else if(de == 180){
+            derection ="S";
+        }else if(180 <de && de <270){
+            derection ="SW";
+        }else if(de == 270){
+            derection ="W";
+        }else {
+            derection ="NW";
+        }
+        return derection;
     }
 }
